b15ae94a8d978743d2d8ad7de6ccaac26623cf5e
oloref <samping2money@gmail.com>
2023-02-06 17:36:34 +0700

    добавлен стартовый прелоадер

--- app/js/main.js
+++ app/js/main.js
@@ -86,6 +86,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		});
 	});
 
+	document.querySelector('.can__item-circle').classList.add('magictime', 'fadeIn');
 
-	document.querySelector('.can__item-circle').classList.add('magictime', 'fadeIn')
+	// preloader
+	function loadData() {
+		return new Promise((resolve, reject) => {
+		  // setTimeout не является частью решения
+		  // Код ниже должен быть заменен на логику подходящую для решения вашей задачи
+		  setTimeout(resolve, 2000);
+		})
+	  }
+	  
+	  loadData()
+		.then(() => {
+		  let preloaderEl = document.getElementById('preloader');
+		  preloaderEl.classList.add('hidden');
+		  preloaderEl.classList.remove('visible');
+	});
 });
